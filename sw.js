ed21d2ed838a06964fe1289902d69e61c2997c20
emrctinkaya <emre.cetinkaya92@gmail.com>
2026-07-29 22:50:36 +0200

    Refresh PWA cache after localization fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hamburg-park-v1.7.0';
+const CACHE='hamburg-park-v1.7.1';
 const SHELL=['./','./index.html','./manifest.json','./icon.svg','./localization-fix.js','./onboarding-ui.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
