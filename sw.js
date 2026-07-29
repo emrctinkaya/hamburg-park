0ed1eac3cbded8c33c9413c3c3670a7562ea9dd6
emrctinkaya <emre.cetinkaya92@gmail.com>
2026-07-29 11:22:46 +0200

    Bump PWA cache for v1.3.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hamburg-park-v1.2.0';
+const CACHE='hamburg-park-v1.3.0';
 const SHELL=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
