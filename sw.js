794d19641861f118865757dda0b52b83e308d170
emrctinkaya <emre.cetinkaya92@gmail.com>
2026-08-17 00:19:04 +0200

    Refresh cache for permit dropdown fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hamburg-park-v1.8.5';
+const CACHE='hamburg-park-v1.8.6';
 const SHELL=['./','./index.html','./manifest.json','./icon.svg','./localization-fix.js','./onboarding-ui.js','./live-location-fix.js','./flag-language-picker.js','./ux-polish.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
